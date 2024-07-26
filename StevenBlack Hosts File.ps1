cc30c695f5de4749f63d80664712d05122dc3f0d
JustIndy <138534560+JustinIndy@users.noreply.github.com>
2024-07-26 17:22:47 -0400

    StevenBlack Hosts File.ps1

--- StevenBlack Hosts File.ps1
+++ StevenBlack Hosts File.ps1
@@ -1,3 +1,4 @@
+:: By JustIndy
 Invoke-WebRequest "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" -OutFile "C:\Windows\System32\drivers\etc\hosts"
 ipconfig /flushdns
 reg add "HKLM\SYSTEM\CurrentControlSet\services\Dnscache\Parameters" /v "MaxNegativeCacheTtl" /t "REG_DWORD" /d "0" /f
