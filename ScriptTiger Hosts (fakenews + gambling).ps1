129fcd19a657c168680d859936b94ddf6b1d223b
JustIndy <138534560+JustinIndy@users.noreply.github.com>
2024-07-26 17:22:00 -0400

    ScriptTiger Hosts (fakenews + gambling).ps1

--- ScriptTiger Hosts (fakenews + gambling).ps1
+++ ScriptTiger Hosts (fakenews + gambling).ps1
@@ -1,3 +1,4 @@
+:: By JustIndy
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
