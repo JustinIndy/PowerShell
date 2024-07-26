d1cc68c6093b4e0de6045979c7abb18f7da4c55d
JustIndy <138534560+JustinIndy@users.noreply.github.com>
2024-07-26 17:23:32 -0400

    VPN.AC OVPN Config.ps1

--- VPN.AC OVPN Config.ps1
+++ VPN.AC OVPN Config.ps1
@@ -1,3 +1,4 @@
+:: By JustIndy
 New-Item -ItemType Directory -Force -Path "~\OpenVPN\config"
 Invoke-WebRequest "https://vpn.ac/ovpn/AES-256-UDP/us-east_ashburn-aes256-udp.ovpn" -OutFile "~\OpenVPN\config\US East, Ashburn.ovpn"
 Invoke-WebRequest "https://vpn.ac/ovpn/AES-256-UDP/canada_toronto-aes256-udp.ovpn" -OutFile "~\OpenVPN\config\Canada, Toronto (P2P).ovpn"
